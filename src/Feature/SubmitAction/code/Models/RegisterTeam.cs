4b294c1ce7ba5721def88b0bf9e7e087e28dad99
João Beleza <b3l3za@gmail.com>
2020-02-29 22:16:32 +0000

    .

---
+++ src/Feature/SubmitAction/code/Models/RegisterTeam.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Web;
+using System.Web.Security;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceForms.Models;
+using Sitecore.ExperienceForms.Processing;
+using Sitecore.ExperienceForms.Processing.Actions;
+using Sitecore.SecurityModel;
+
+namespace Hackathon.Feature.RegisterTeam.Models
+{
+    public class RegisterTeam : SubmitActionBase<string>
+    {
+        private HttpClient client;
+        HttpResponseMessage response;
+        private static string fileName;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogSubmit"/> class.
+        /// </summary>
+        /// <param name="submitActionData">The submit action data.</param>
+        public RegisterTeam(ISubmitActionData submitActionData) : base(submitActionData)
+        {
+        }
+
+        /// <summary>
+        /// Tries to convert the specified <paramref name="value" /> to an instance of the specified target type.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="target">The target object.</param>
+        /// <returns>
+        /// true if <paramref name="value" /> was converted successfully; otherwise, false.
+        /// </returns>
+        protected override bool TryParse(string value, out string target)
+        {
+            target = string.Empty;
+            return true;
+        }
+
+        /// <summary>
+        /// Executes the action with the specified <paramref name="data" />.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <param name="formSubmitContext">The form submit context.</param>
+        /// <returns>
+        ///   <c>true</c> if the action is executed correctly; otherwise <c>false</c>
+        /// </returns>
+        protected override bool Execute(string data, FormSubmitContext formSubmitContext)
+        {
+            Assert.ArgumentNotNull(formSubmitContext, nameof(formSubmitContext));
+            Dictionary<string, object> values = new Dictionary<string, object>();
+            foreach (IViewModel fields in formSubmitContext.Fields)
+            {
+                try
+                {
+                    KeyValuePair<string, object> fieldData = GetFieldData(fields);
+                    values.Add(fieldData.Key, fieldData.Value);
+                }
+                catch (Exception ex)
+                {
+                    Sitecore.Diagnostics.Log.Error(ex.Message, ex, typeof(RegisterTeam));
+                }
+
+            }
+
+            //Disable the Security
+            using (new SecurityDisabler())
+            {
+                Database master = Sitecore.Configuration.Factory.GetDatabase("master");
+                if (master != null)
+                {
+                    //Here you need the Parent Item (new item will be child of this item)
+                    Sitecore.Data.Items.Item parentItem = master.GetItem(new ID("{F9B025E2-42E6-489A-98A8-F660834062E2}"));
+                    
+                    //Get the template for the new item
+                    TemplateItem teamTemplate = master.GetTemplate(Templates.Team.Id);
+
+                    if (teamTemplate != null)
+                    {
+                        //Now, use Parent item to add a new item underneath and pass the item name and template as parameter
+                        Item newItem = parentItem?.Add("NewItem", teamTemplate);
+
+                        try
+                        {
+                            if (newItem != null)
+                            {
+                                newItem.Editing.BeginEdit();
+                                newItem.Editing.EndEdit();
+                            }
+                        }
+                        catch
+                        {
+                            newItem.Editing.CancelEdit();
+                        }
+
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public string GetStatus(string data)
+        {
+            var datan = data;
+            return datan;
+        }
+        public static KeyValuePair<string, object> GetFieldData(IViewModel postedField)
+        {
+            Assert.ArgumentNotNull((object)postedField, "postedField");
+            IValueField valueField = postedField as IValueField;
+
+            if ((valueField != null ? (valueField.AllowSave ? 1 : 0) : 0) == 0)
+                return GenerateKeyValuePair(postedField.Name);
+
+            var fileProperty = postedField.GetType().GetProperty("File");
+
+            var file = fileProperty?.GetValue(postedField);
+
+            if (fileProperty == null)
+            {
+                PropertyInfo property2 = postedField.GetType().GetProperty("Value");
+                object postedValue2;
+                postedValue2 = property2.GetValue((object)postedField);
+
+                return GenerateKeyValuePair(postedField.Name, postedValue2);
+            }
+            else
+            {
+                if (file != null)
+                {
+                    var property = ((HttpPostedFileWrapper)fileProperty.GetValue(postedField)).InputStream;
+                    fileName = ((HttpPostedFileWrapper)fileProperty.GetValue(postedField)).FileName;
+                    var propertylenght = ((HttpPostedFileWrapper)fileProperty.GetValue(postedField)).ContentLength;
+                    byte[] fileData = null;
+                    using (var binaryReader = new BinaryReader(property))
+                    {
+                        fileData = binaryReader.ReadBytes(propertylenght);
+                    }
+                    object postedValue;
+                    postedValue = fileData;
+                    return GenerateKeyValuePair(postedField.ItemId, postedValue);
+                }
+                else
+                {
+                    return GenerateKeyValuePair(postedField.ItemId, null);
+                }
+            }
+        }
+
+        public static Dictionary<string, T> GetFieldsDictionary<T>(IList<IViewModel> fields)
+        {
+            Dictionary<string, T> values = new Dictionary<string, T>();
+            foreach (IViewModel field in fields)
+            {
+                KeyValuePair<string, object> fieldData = GetFieldData(field);
+                values.Add(fieldData.Key, (T)fieldData.Value);
+            }
+            return values;
+
+        }
+
+        private string GenerateJSONWebToken(Dictionary<string, object> userInfo, string password)
+        {
+            // Define const Key this should be private secret key  stored in some safe place
+            Guid key = Guid.NewGuid();
+            string keySend = "";
+
+            keySend = "key=" + key.ToString();
+            Sitecore.Security.SitecoreMembershipProvider provider = new Sitecore.Security.SitecoreMembershipProvider();
+            MembershipCreateStatus status = MembershipCreateStatus.Success;
+            Membership.CreateUser(@"portal\" + userInfo["Nif"], password, userInfo["Email"].ToString(), userInfo["Pergunta"].ToString(), userInfo["Resposta"].ToString(), false, out status);
+            if (status != MembershipCreateStatus.Success)
+                throw new MembershipCreateUserException(status);
+
+            return keySend;
+        }
+        public static void UpdateUserKeyActivation(string userName, Guid key)
+        {
+            Sitecore.Security.Accounts.User user = Sitecore.Security.Accounts.User.FromName(userName, true);
+            user.Profile.SetCustomProperty("KeyActivation", key.ToString());
+            user.Profile.Save();
+        }
+
+
+        public static KeyValuePair<string, object> GenerateKeyValuePair(string key, object value = null)
+        {
+            return new KeyValuePair<string, object>(key, value);
+        }
+    }
+}
--- src/Feature/SubmitAction/code/Templates.cs
+++ src/Feature/SubmitAction/code/Templates.cs
@@ -4,9 +4,45 @@ using System.Linq;
 using System.Web;
 using Sitecore.Data;
 
-namespace Hackaton.Feature.SubmitAction
+namespace Hackathon.Feature.RegisterTeam
 {
-  public struct Templates
-  {
-  }
+    public struct Templates
+    {
+
+        public struct Team
+        {
+            public static readonly ID Id = new ID("{25D09680-58D4-4E81-8B44-C26E713F8171}");
+
+            public struct Fields
+            {
+                public static readonly ID Name = new ID("{C9325C96-70AA-48E3-9974-9DE87099A778}");
+                public static readonly ID Description = new ID("{5BAF8ACB-EE08-4BB9-A568-BE472410A2BD}");
+                public static readonly ID Logo = new ID("{19F1730F-1672-4B72-B583-FF7F8DAB1ED5}");
+                public static readonly ID SubmissionDate = new ID("{343635C8-B0EF-4603-95D8-F5C9310FCE2F}");
+                public static readonly ID VideoLink = new ID("{A3B1C704-1A67-4319-B81A-40690E984C2B}");
+                public static readonly ID ProjectRepository = new ID("{26591FEA-69ED-4791-AF00-56BCD3BD062B}");
+                public static readonly ID ProjectDescription = new ID("{09EEBF3A-C99D-49DF-96BA-BDA556A9664D}");
+            }
+
+        }
+
+        public struct Person
+        {
+            public static readonly ID Id = new ID("{B3F46401-62EA-4FD9-A409-27BE21150A1F}");
+
+            public struct Fields
+            {
+                public static readonly ID Name = new ID("{8F95C9AB-3019-4091-9D0E-9F22DC31648D}");
+                public static readonly ID Country = new ID("{5660AAA1-6EFE-41AF-B852-0E47870CA926}");
+                public static readonly ID LinkedIn = new ID("{240337B8-1CFB-41AA-A0AD-6AB78A69F0F2}");
+                public static readonly ID Facebook = new ID("{E7872B5E-03BF-4FB9-A55F-2A60F9047AB5}");
+                public static readonly ID Instagram = new ID("{A8A0C020-FF3D-432D-9E85-AC7372EA248D}");
+                public static readonly ID Twitter = new ID("{C9A4E3EB-2BB0-448C-8BCF-62A20917AE04}");
+                public static readonly ID IsTeamLeader = new ID("{802E9150-466F-4213-BF6F-00F2814487B1}");
+                public static readonly ID Photo = new ID("{13BD0F9A-36CD-4024-87E8-60EADF0361FA}");
+            }
+
+        }
+
+    }
 }
