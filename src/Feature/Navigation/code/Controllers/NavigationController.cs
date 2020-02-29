4b03409d361e30716aed976cfb48b62aed04cad2
ruben_Marcelino <ruben.a.marcelino@noesis.pt>
2020-02-29 07:30:03 +0000

    Commit

---
+++ src/Feature/Navigation/code/Controllers/NavigationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hackathon.Feature.CustomNavigationController.Controllers
+{
+    public class CustomNavigationController : Controller
+    {
+        // GET: Navigation
+        public ActionResult Index()
+        {
+            return View("navigation");
+        }
+    }
+}
